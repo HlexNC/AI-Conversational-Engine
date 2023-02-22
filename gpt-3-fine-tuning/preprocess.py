3f1140db02481244159c393912b241895a7d2a71
HlexNC <hlexhelftd@gmail.com>
2023-02-22 09:07:55 +0200

    Update preprocess.py

--- gpt-3-fine-tuning/preprocess.py
+++ gpt-3-fine-tuning/preprocess.py
@@ -1,9 +1,11 @@
 import boto3
+import whisper
 
 s3 = boto3.resource(
     service_name='s3',
     region_name='eu-west-1',
 )
+model = whisper.load_model("base")
 
 bucket = s3.Bucket('recordings3cx')
 bucket_path = 'RU/7520/'
@@ -15,7 +17,11 @@ for obj in bucket.objects.filter(Prefix=bucket_path):
         continue
     else:
         mp3_content = obj.get()['Body'].read()
-        with open(output_file, 'wb') as f:
-            f.write(mp3_content)
+    result = model.transcribe(mp3_content)
+    print(result["text"])
+
+
 
 # Cannot save to another dir using "data/" + output_file
+# need to install torch
+# can't install torch cuz of 3.11
