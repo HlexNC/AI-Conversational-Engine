fb36c365136d3bd1f0e77bfc6ae52b984ff393e3
HlexNC <hlexhelftd@gmail.com>
2023-03-02 20:53:40 +0200

    Comments added

--- gpt-3-fine-tuning/preprocess.py
+++ gpt-3-fine-tuning/preprocess.py
@@ -5,6 +5,7 @@
 @audio_to_text: Converts an audio file to text using OpenAI's Audio.transcribe method.
 @config: Loads the environment variables for OpenAI API key and AWS credentials.
 """
+# Imports
 import boto3
 import openai
 from pydub import AudioSegment
@@ -12,9 +13,12 @@ import os
 from dotenv import load_dotenv
 import datetime
 
+# Global variables
 bucket = None
 bucket_path = None
 
+
+# A function to retrieve the object from the bucket and process it
 def config():
     load_dotenv()
     openai.api_key = os.getenv('openai_key')
@@ -30,12 +34,14 @@ def config():
     bucket_path = 'RU/7520/'
 
 
+# A function to convert the audio file to mp3
 def convert_to_mp3(filename):
     sound = AudioSegment.from_wav(filename)
     sound.export(filename[:-4] + ".mp3", format="mp3")
     # print(f"{datetime.datetime.now()} Success conversion to mp3")
 
 
+# A function to convert the audio file to text
 def audio_to_text(filename):
     mp3_filename = filename[:-4] + ".mp3"
     # print(f"\{{datetime.datetime.now()}} working with " + mp3_filename)
@@ -47,6 +53,7 @@ def audio_to_text(filename):
     return transcript
 
 
+# The main function
 def main():
     config()
     for obj in bucket.objects.filter(Prefix=bucket_path):
@@ -65,5 +72,6 @@ def main():
             # return result
 
 
+# Run the main function
 if __name__ == "__main__":
     main()
